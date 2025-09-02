a8cac1ea9f8196d17435e7e3d7963297dd984b9b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-09-02 23:09:02 +0530

    Add about route

--- Gulsan_Sir/Backend/my-node-app/index.js
+++ Gulsan_Sir/Backend/my-node-app/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express(); // Define a route for the root URL
-app.get('/', (req, res) => {res.send('Hello World from Express!');});
+
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World from Express!");
+});
+
+app.get("/about", (req, res) => {
+    res.send("This is the About Page");
 });
