7ed0bf85a4b915605fc37a6d1544b36842c8b60c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-09-27 14:47:45 +0530

    refactor Main.kt: enhance main function with variable declarations, control structures, and prime number check

--- Ramjan_Sir/Kotlin/src/Main.kt
+++ Ramjan_Sir/Kotlin/src/Main.kt
@@ -1,14 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    // Variable declaration and printing
+    val data = "Welcome"
+    println(data)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    var data1 = "9 AM changeable"
+    println(data1)
+
+    var data2: Int = 25
+    println(data2)
+
+    // 'when' expression example
+    val a = 5
+    val result = when (a) {
+        1 -> "One"
+        2 -> "Two"
+        3 -> "Three"
+        4 -> "Four"
+        5 -> "Five"
+        else -> "Wrong Number."
     }
+    println(result)
+
+    // Array access
+    val arr = arrayOf(1, 2, 3, 4)
+    println(arr[1])
+
+    // Loops: 'for' with ranges
+    for (i in 1..7) {
+        print(i)
+    }
+    println()
+
+    for (i in 1 until 5) {
+        print(i)
+    }
+    println()
+
+    // Prime number check
+    val num = 5
+    var isPrime = true
+
+    if (num <= 1) {
+        isPrime = false
+    } else {
+        for (i in 2 until num) {
+            if (num % i == 0) {
+                isPrime = false
+                break
+            }
+        }
+    }
+
+    if (isPrime) {
+        println("Number is prime")
+    } else {
+        println("Number is not prime")
+    }
+
+    // Single-expression function
+    fun add(a: Int, b: Int) = a + b
+    val r = add(2, 5)
+    println("The sum is $r") // Corrected print statement
+
+    // Overloaded functions
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sum(a: Int, b: Int, c: Int): Int {
+        return a + b + c
+    }
+
+    val r1 = sum(50, 60)
+    val r2 = sum(100, 200, 300)
+    println(r1)
+    println(r2)
 }
