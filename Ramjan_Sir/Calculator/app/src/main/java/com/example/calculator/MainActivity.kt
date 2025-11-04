b449aee3aa43feeb3f2f3c90258dc36d71d62d31
Kaushik Mandal <koushik369mondal@gmail.com>
2025-11-04 19:41:53 +0530

    Update .gitignore and modify MainActivity layout for improved structure and functionality

--- Ramjan_Sir/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Ramjan_Sir/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,54 +1,20 @@
 package com.example.calculator
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var e1: EditText
-    lateinit var e2: EditText
-    lateinit var b1: Button
-    lateinit var b2: Button
-    lateinit var b3: Button
-    lateinit var b4: Button
-    lateinit var bs: TextView
-
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        e1 = findViewById(R.id.num1)
-        e2 = findViewById(R.id.num2)
-        b1 = findViewById(R.id.btn1)
-        b2 = findViewById(R.id.btn2)
-        b3 = findViewById(R.id.btn3)
-        b4 = findViewById(R.id.btn4)
-        bs = findViewById(R.id.btnSubmit)
-
-        b1.setOnClickListener(this)
-        b2.setOnClickListener(this)
-        b3.setOnClickListener(this)
-        b4.setOnClickListener(this)
-        bs.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        // handle button clicks
-        var a = e1.text.toString().toDouble()
-        var b = e2.text.toString().toDouble()
-        var result: Double = 0.0
-        when(v?.id) {
-            R.id.btn1 -> result = a / b
-            R.id.btn2 -> result = a + b
-            R.id.btn3 -> result = a - b
-            R.id.btn4 -> result = a * b
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-        bs.text = "Result is : $result"
-
     }
 }
